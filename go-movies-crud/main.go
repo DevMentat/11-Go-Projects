7a982fbd68a0ce00861b9b663e4d096b451d11bd
DevMentat <devmentat@proton.me>
2022-10-19 13:41:51 +0200

    Adding port info + comments

--- go-movies-crud/main.go
+++ go-movies-crud/main.go
@@ -1,7 +1,14 @@
 package main
 
-import "github.com/gorilla/mux"
+import (
+	"fmt"
+	"log"
+	"net/http"
 
+	"github.com/gorilla/mux"
+)
+
+// Define structures
 type Movie struct {
 	ID       string    `json:"ID"`
 	Isbn     string    `json:"isbn"`
@@ -14,15 +21,19 @@ type Director struct {
 	Lastname  string `json:"lastname"`
 }
 
+// Define variables
 var movies []Movie
 
+// Main function
 func main() {
 	r := mux.NewRouter()
-
+	// 5 routes
 	r.HandleFunc("/movies", getMovies).Methods("GET")
 	r.HandleFunc("/movies/{id}", getMovie).Methods("GET")
 	r.HandleFunc("/movies", createMovie).Methods("POST")
 	r.HandleFunc("/movies/{id}", updateMovie).Methods("PUT")
 	r.HandleFunc("/movies/{id}", deleteMovies).Methods("DELETE")
-
+	// Server print
+	fmt.Println("Starting server at port 8080")
+	log.Fatal(http.ListenAndServe(":8080", r))
 }
